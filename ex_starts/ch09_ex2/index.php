dd81c55e2d5cae0526c5f37e51109d6db0ac012a
munsta2 <79557328+munsta2@users.noreply.github.com>
2022-03-19 14:19:29 -0400

    ex 9 done

--- ex_starts/ch09_ex2/index.php
+++ ex_starts/ch09_ex2/index.php
@@ -13,34 +13,53 @@ switch ($action) {
         $number2 = filter_input(INPUT_POST, 'number2');
         $number3 = filter_input(INPUT_POST, 'number3');
 
-        // make sure the user enters three numbers
-
-        // make sure the numbers are valid
-
-        // get the ceiling and floor for $number2
-
-        // round $number3 to 3 decimal places
-
-        // get the max and min values of all three numbers
-
-        // generate a random integer between 1 and 100
-
-        // format the message
-        $message = "This page is under construction.\n" .
-                   "Please write the code that processes the data.";
-        //$message =
-        //    "Number 1: $number1\n" .
-        //    "Number 2: $number2\n" .
-        //    "Number 3: $number3\n" .
-        //    "\n" .
-        //    "Number 2 ceiling: $number2_ceil\n" .
-        //    "Number 2 floor: $number2_floor\n" .
-        //    "Number 3 rounded: $number3_rounded\n" .
-        //    "\n" .
-        //    "Min: $min\n" .
-        //    "Max: $max\n" .
-        //    "\n" .
-        //    "Random: $random\n";
+        //are three numbers inputted?
+        if(empty($number1) ||
+            empty($number2) ||
+            empty($number3)){
+            $message = 'Please enter in all three numbers.';
+            break;
+        }    
+        
+        //are the numbers valid
+        if(!is_numeric($number1) ||
+                !is_numeric($number2) ||
+                !is_numeric($number3)){
+            $message = 'You must enter three valid numbers.';
+            break;
+        }
+        
+        //ceiling and floor for #2
+        $ceil = ceil($number2);
+        $floor = floor($number2);
+        
+        //round to 3 decimal places
+        $number3_round = round($number3, 3);
+        
+        //min value
+        $min = min($number1, $number2, $number3);
+        
+        //max value 
+        $max = max($number1, $number2, $number3);
+        
+        //random number form 1-100
+        $random = mt_rand(1, 100);
+        
+        //output upon submit
+        $message =
+            "Number 1: $number1\n" .
+            "Number 2: $number2\n" .
+            "Number 3: $number3\n" .
+            "\n" .
+            "Number 2 ceiling: $ceil\n" .
+            "Number 2 floor: $floor\n" .
+            "Number 3 rounded: $number3_round\n" .
+            "\n" .
+            "Min: $min\n" .
+            "Max: $max\n" .
+            "\n" .
+            "Random: $random\n";
+            
 
         break;
 }
